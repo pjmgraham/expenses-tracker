24e1ff3c4c6b3ba4d7291fa4febc2d2819ff8a81
Patrick Graham <95481954+pjmgraham@users.noreply.github.com>
2022-08-26 23:52:03 +0200

    added bar component

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -19,7 +19,6 @@ const ExpenseItem = (props) => {
           <h2>{props.title}</h2>
           <div className="expense-item__price">£{props.amount}</div>
         </div>
-        <button onClick={clickHandler}>CLICK ME</button>
       </Card>
     </li>
   );
